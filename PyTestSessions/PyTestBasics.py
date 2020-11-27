0c92f6d05ae79b06c69c36b6f9e62a79caad4e15
Keshini <kdpatel8184@gmail.com>
2020-11-26 17:18:46 -0700

    This is first commit.

---
+++ PyTestSessions/PyTestBasics.py
@@ -0,0 +1,9 @@
+import pytest
+
+# All the test should start with test keyword or
+# All should end with test keyword
+
+def test_m11():
+    name = 'abc'
+    assert name.upper() == "SELENIUM"
+
---
+++ PyTestSessions/Test_demo3.py
@@ -0,0 +1,14 @@
+def test_m4():
+    assert False
+
+
+def test_m5():
+    assert 100 == 100
+
+
+def test_m6():
+    assert "Testing" == "TESTING"
+
+
+def test_login_gmail():
+    assert "admin" == "admin"
